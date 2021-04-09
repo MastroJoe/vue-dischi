fea6d2bc4c897816247a801da20e3586003a4d23
Giorgio <giorgio.pignattari@gmail.it>
2021-04-09 12:50:57 +0200

    inizio struttura html

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Descrizione
+// Attraverso una chiamata ajax all’API di boolean https://flynn.boolean.careers/exercises/api/array/music
+// avremo a disposizione una decina di dischi musicali.
+// Utilizzando vue, stampiamo a schermo una card per ogni album.
+// BONUS
+// Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
+// BONUS 2
+// Ordinare i dischi per anno di uscita.
+
+Vue.config.devtools = true;
+
+var app = new Vue(
+  {
+    el: '#root',
+    data: {
+
+    },
+  }
+);
