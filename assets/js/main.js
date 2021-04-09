a7fa8878b83a3af3fe53204d4a097ed1cb42830e
Giorgio <giorgio.pignattari@gmail.it>
2021-04-09 16:57:30 +0200

    prova flip con 1 oggetto

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,7 +13,15 @@ var app = new Vue(
   {
     el: '#root',
     data: {
-
-    },
+      albums: [
+        {
+          "poster": "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+          "title": "New Jersey",
+          "author": "Bon Jovi",
+          "genre": "Rock",
+          "year": "1988"
+        },
+      ]
+    }
   }
 );
